4532e5f10b2996c9ee62d92865ba86da1087070f
Alexander Bukley <alexanderbukley@Alexanders-MacBook-Pro.local>
2023-10-11 22:07:08 +0200

    [FTR] add sorting to items list

--- src/app/pages/items-list/state/items.selectors.ts
+++ src/app/pages/items-list/state/items.selectors.ts
@@ -1,5 +1,18 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import { Item } from '../interfaces/item';
 
-export const selectItems = createFeatureSelector<ReadonlyArray<Item>>('items');
+export const selectItems = createSelector(
+  createFeatureSelector<ReadonlyArray<Item>>('items'),
+  (items) => {
+    return [...items].sort((a, b) => {
+      if (a.isContainer && !b.isContainer) {
+        return -1;
+      } else if (!a.isContainer && b.isContainer) {
+        return 1;
+      }
+
+      return a.description.localeCompare(b.description);
+    });
+  },
+);
